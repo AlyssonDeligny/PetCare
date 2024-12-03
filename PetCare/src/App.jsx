39331a1222f90dd1dc40a32c22a0bce22bb0d87b
Alysson <alysson.deligny@gmail.com>
2024-12-03 11:45:59 +0100

    ajout image

--- PetCare/src/App.jsx
+++ PetCare/src/App.jsx
@@ -1,35 +1,71 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import avatar from "./assets/smarties.jpg";
 
-function App() {
-  const [count, setCount] = useState(0)
+const user = {
+  name: "Hedy Lamarr",
+  imageUrl: "https://i.imgur.com/yXOvdOSs.jpg",
+  imageSize: 90,
+};
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+function MyButton() {
+  return <button>Je suis un bouton</button>;
 }
 
-export default App
+function AboutPage() {
+  return (
+    <>
+      <h1>À propos</h1>
+      <p>
+        Bien le bonjour.
+        <br />
+        Comment ça va ?
+      </p>
+      <img className="avatar" src={avatar} alt="Mon avatar"></img>
+      <h1>{user.name}</h1>
+      <img className="avatar" src={user.imageUrl} />
+    </>
+  );
+}
+
+export default function MyApp() {
+  return (
+    <div>
+      <h1>Bienvenue dans mon appli</h1>
+      <MyButton />
+      <AboutPage />
+    </div>
+  );
+}
+// function App() {
+//   const [count, setCount] = useState(0);
+
+//   return (
+//     <>
+//       <div>
+//         <a href="https://vite.dev" target="_blank">
+//           <img src={viteLogo} className="logo" alt="Vite logo" />
+//         </a>
+//         <a href="https://react.dev" target="_blank">
+//           <img src={reactLogo} className="logo react" alt="React logo" />
+//         </a>
+//       </div>
+//       <h1>Vite + React</h1>
+//       <div className="card">
+//         <button onClick={() => setCount((count) => count + 1)}>
+//           count is {count}
+//         </button>
+//         <p>
+//           Edit <code>src/App.jsx</code> and save to test HMR
+//         </p>
+//       </div>
+//       <p className="read-the-docs">
+//         Click on the Vite and React logos to learn more
+//       </p>
+//     </>
+//   );
+// }
+
+// export default App;
